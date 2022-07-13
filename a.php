4932deecbd6da40db2a269bbde12a2af689f1354
平井 一輝 <h355hira@ssh.cs.ehime-u.ac.jp>
2022-07-13 11:17:50 +0900

    !==false

--- a.php
+++ a.php
@@ -4,7 +4,9 @@
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
     //if(strpos($line, "abc") == true)
     //   echo $line;
-    if(strpos($line, "abc") === false){} // どこにも"abc" が含まれていないとfalseを返す
-    else echo "$line";
+    //    if(strpos($line,"abc") === false){} // どこにも"abc" が含まれていないとfalseを返す
+      if(strpos($line, "abc") !== false){
+    		echo "$line";
+	 }   
   }
 ?>
